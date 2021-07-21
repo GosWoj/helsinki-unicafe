836de5cf40ecdabd6b91af69677eb7f4be8938a4
GosWoj <gosiawoojcik@gmail.com>
2021-07-21 15:05:50 +0200

    step2 done

--- src/App.js
+++ src/App.js
@@ -8,17 +8,24 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const handleGood = () => {
     setGood(good + 1);
+    setAll(all + 1);
+    setAverage(average + 1);
   };
 
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   };
 
   const handleBad = () => {
     setBad(bad + 1);
+    setAll(all + 1);
+    setAverage(average - 1);
   };
 
   return (
@@ -31,6 +38,9 @@ function App() {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average / all}</p>
+      <p>Positive: {(good / all) * 100} %</p>
     </div>
   );
 }
